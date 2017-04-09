7fa3d06262499735ed7a405eea3d4ae494dfa068
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-09 17:21:54 +0300

    cap deploy with Instance profile

--- Gemfile
+++ Gemfile
@@ -26,9 +26,10 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'capistrano'
-  gem 'capistrano3-puma', require: false
-  gem 'capistrano-rails', require: false
-  gem 'capistrano-bundler', require: false
+  gem 'capistrano3-puma'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
   gem 'capistrano-rvm'
-  
+  gem 'capistrano3-nginx'
+  gem 'capistrano-upload-config'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,14 @@ set :branch, :capistrano
 set :deploy_to, '/home/deploy/basecamp3-rails-chatbot'
 set :pty, true
 set :linked_files, %w{config/application.yml}
+
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.3.1' # Edit this if you are using MRI Ruby
@@ -16,7 +23,10 @@ set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
+#set :puma_conf, "#{shared_path}/puma.rb"
+
+set :puma_conf, "#{shared_path}/config/puma.rb"
+
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
@@ -26,6 +36,17 @@ set :puma_workers, 1
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, false
+
+ 
+namespace :deploy do
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+  after 'puma:smart_restart', 'nginx:restart'
+end
+
+
+
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
